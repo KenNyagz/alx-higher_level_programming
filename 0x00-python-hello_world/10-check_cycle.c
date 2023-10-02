1fddcb53709a058a9a839716b91d4170699a2558
KenNyagz <kennyagz681@gmail.com>
2023-10-03 00:22:02 +0300

    Header file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#inlcude "lists.h"
+
+/**
+*check_cycle - check for a cycle in a linked list
+*@list: pointer to head of linked list
+*Return: Number of odes before loop
+*/
+
+int check_cycle(listint_t *list)
+{
+
+
+
+}
