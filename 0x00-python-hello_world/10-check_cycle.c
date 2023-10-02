02623738fc698f355a683ca7d9c204cf98828186
KenNyagz <kennyagz681@gmail.com>
2023-10-03 00:40:16 +0300

    Check loop in linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,7 +8,15 @@
 
 int check_cycle(listint_t *list)
 {
+listint_t *fast = list, *slow = list;
+int count = 0;
 
-
-
+	while (fast->head != NULL && fast != NULL)
+	{
+		fast = list->head->head;
+		slow = list->head;
+		if (slow == fast)
+			return (1);
+	}
+return (0);
 }
