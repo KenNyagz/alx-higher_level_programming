2680f176048e26f15cea206e079ba1e1382a17fa
KenNyagz <kennyagz681@gmail.com>
2023-10-03 19:24:38 +0300

    An aliased Commit to save time

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,10 +7,12 @@ else:
     numbercp = number
 
 modlo = numbercp % 10
+
 print(f"Last digit of {number} is {modlo}", end=' ')
 if modlo == 0:
     print("and is 0")
-elif modlo < 6 and modlo != 0:
-    print("and is less than 6 and not 0")
-else:
+elif modlo > 5:
     print("and is greater than 5")
+else:
+    print("and is less than 6 and not 0")
+
