a531164d45d5328eede7cc5dbdd0e8e46f89d740
KenNyagz <kennyagz681@gmail.com>
2023-10-03 16:45:34 +0300

    More control flow

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if (number < 0):
+if number < 0:
     numbercp = -number
 else:
     numbercp = number
@@ -10,7 +10,7 @@ modlo = numbercp % 10
 
 if modlo is 0:
      print("Last digit of {number} is {modlo} and is 0")
-elif modlo < 6 and not 0:
+elif modlo < 6 and modlo != 0:
      print("Last digit of {number} is {modlo} and is less than 6 and not 0"
 else:
      print("Last digit of {number} is {modlo} and is greater than 5"
