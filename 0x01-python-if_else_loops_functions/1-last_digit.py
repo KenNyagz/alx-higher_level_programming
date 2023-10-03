357b7d02e7c05837da09b10dff02835a04a98479
KenNyagz <kennyagz681@gmail.com>
2023-10-03 19:38:01 +0300

    An aliased Commit to save time

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
-    numbercp = -number
-else:
-    numbercp = number
 
-modlo = numbercp % 10
+modlo = abs(number) % 10
 
 print(f"Last digit of {number} is {modlo}", end=' ')
 if modlo == 0:
@@ -15,4 +11,3 @@ elif modlo > 5:
     print("and is greater than 5")
 else:
     print("and is less than 6 and not 0")
-
