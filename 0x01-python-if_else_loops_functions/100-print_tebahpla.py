6facf8ca7c7926b4088bf6a0376daddbf9ef1b1a
KenNyagz <kennyagz681@gmail.com>
2023-10-04 00:50:52 +0300

    An aliased Commit to save time

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(90, 64, -1):
-    print("{}{}".format(chr(i).lower(), chr(i - 33)), end='')
+for i in range(122, 96, -1):
+    print("{}{}".format(chr(i), chr(i - 32)), end='')
