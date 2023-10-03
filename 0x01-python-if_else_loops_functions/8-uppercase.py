69652de07c8282889e1e955e3e14bc453a6f72e9
KenNyagz <kennyagz681@gmail.com>
2023-10-04 00:31:39 +0300

    An aliased Commit to save time

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,4 +5,4 @@ def uppercase(input_str):
     for char in input_str:
         upper_code = ord(char) - 32 if 97 <= ord(char) <= 122 else ord(char)
         print(chr(upper_code), end='')
-    print("")  # Print a new line after the loop
+    print("")
