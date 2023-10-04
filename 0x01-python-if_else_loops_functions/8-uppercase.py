2c611468e45c4e3164158a2eb5aa13b7af2db6ee
KenNyagz <kennyagz681@gmail.com>
2023-10-04 23:20:41 +0300

    An aliased Commit to save time

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def to_uper(character):
     if ord(character) >= 97 and ord(character) <= 122:
-       return (ord(character) - 32)
+        return (ord(character) - 32)
     else:
         return ord(character)
 
