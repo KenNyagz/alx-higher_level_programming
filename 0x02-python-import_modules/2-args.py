300b3cea05b599661726a6033940849d12453a7a
KenNyagz <kennyagz681@gmail.com>
2023-10-05 18:06:23 +0300

    An aliased Commit to save time

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) - 1 == 0:
-         print("0 arguments.")
-        
+        print("0 arguments.")
+    
     elif len(sys.argv) - 1 == 1:
-         print("1 argument:")
-        
+        print("1 argument:")
+  
     else:
-         print(len(sys.argv) - 1, "argumenets:")
-        
+        print(len(sys.argv) - 1, "argumenets:")
+
     for idx in range(1, len(sys.argv)):
-         print("{}: {}".format(idx, sys.argv[idx]))
+        print("{}: {}".format(idx, sys.argv[idx]))
