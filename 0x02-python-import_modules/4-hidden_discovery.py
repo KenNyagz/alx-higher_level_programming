80e0cc4f17341ddeb583ae3383918653e6d1c944
KenNyagz <kennyagz681@gmail.com>
2023-10-05 23:54:59 +0300

    Reading data in a compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import imp
+    import importlib.util
     import sys
 
-    file_object, module_path, description = imp.find_module('hidden_4')
-    module = imp.load_module('hidden_4', file_object, module_path, description)
+    module_name = 'hidden_4'
+    module_path = './hidden_4.pyc'  # Provide the path to the hidden_4.pyc file
+    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
 
     names = dir(module)
 
