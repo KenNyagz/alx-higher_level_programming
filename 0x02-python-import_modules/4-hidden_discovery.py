e1f9d47d3d7339b0e6ee9ee518c37e74fd1cc454
KenNyagz <kennyagz681@gmail.com>
2023-10-05 23:17:43 +0300

    Reading data in a compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ = "__main__":
+    import imp
+    import sys
+
+    file_object, module_path, description = imp.find_module('hidden_4')
+    module = imp.load_module('hidden_4', file_object, module_path, description)
+
+
+    names = dir(module)
+
+    names = [name for name in names if not name.startswith('__')]
+
+
+     names.sort()
+
+     for name in names:
+         print(name)
