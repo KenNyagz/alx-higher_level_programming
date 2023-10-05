c201896fb7edcb7d60bdec715f0e81606ccdeafd
KenNyagz <kennyagz681@gmail.com>
2023-10-05 23:21:48 +0300

    An aliased Commit to save time

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -7,13 +7,11 @@ if __name__ = "__main__":
     file_object, module_path, description = imp.find_module('hidden_4')
     module = imp.load_module('hidden_4', file_object, module_path, description)
 
-
     names = dir(module)
 
     names = [name for name in names if not name.startswith('__')]
 
+    names.sort()
 
-     names.sort()
-
-     for name in names:
-         print(name)
+    for name in names:
+        print(name)
