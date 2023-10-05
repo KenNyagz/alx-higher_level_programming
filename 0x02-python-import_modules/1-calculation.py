acf7fc7fcc7c6f23b2d8e3ff5a54334a845fcf38
KenNyagz <kennyagz681@gmail.com>
2023-10-05 15:40:16 +0300

    An aliased Commit to save time

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
+    """Print the sum, difference, multiple and division of 10 and 5."""
     import calculator_1
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))                                                                
