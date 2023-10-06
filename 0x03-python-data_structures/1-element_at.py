eef017b5a80414fa7fcbd4debdb9acb96e4da6f1
KenNyagz <kennyagz681@gmail.com>
2023-10-06 22:50:44 +0300

    Access list elem

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    else:
+        elem = my_list[idx]
+        return elem
