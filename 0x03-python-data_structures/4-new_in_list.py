b55ffee92acf49b5be63b76cd079c1b1029484f7
KenNyagz <kennyagz681@gmail.com>
2023-10-07 13:31:46 +0300

    List copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx  < 0:
+        return my_list
+    elif idx > len(my_list) -1:
+        return my_list
+    else:
+        my_listcp = my_list[:]
+        my_listcp[idx] = element
+        return my_listcp
