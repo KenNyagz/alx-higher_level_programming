2012d0a74ae8c399e26e406eae46e18320646b91
KenNyagz <kennyagz681@gmail.com>
2023-10-07 13:40:43 +0300

    An aliased Commit to save time

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+if __name__ = "__main__":
 
-def print_reversed_list_integer(my_list=[]):
-    for num in my_list[::-1]:
-        print("{:d}".format(num))
+    def print_reversed_list_integer(my_list=[]):
+        for num in my_list[::-1]:
+            print("{:d}".format(num))
