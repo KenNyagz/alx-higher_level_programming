8b97bdd11d1b6ec1e4f02575969963eefcef2951
KenNyagz <kennyagz681@gmail.com>
2023-10-09 12:09:12 +0300

    An aliased Commit to save time

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for elem in row:
-            print("{: >2}".format(elem), end=' ')
+            print("{:d}".format(elem), end=' ' if elem != row[-1] else "")
         print()
