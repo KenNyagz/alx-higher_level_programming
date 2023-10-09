d7f4908ff40e9f76f543ca5cf31721d289659c62
KenNyagz <kennyagz681@gmail.com>
2023-10-09 10:02:42 +0300

    An aliased Commit to save time

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+        return
+
+    max_width = max(len(str(element)) for row in matrix for element in row)
     for row in matrix:
         for elem in row:
-            print("{:d}".format(elem), end=' ')
-        print('')
+            print("{:>{}}".format(elem, max_width), end=' ')
+        print()
