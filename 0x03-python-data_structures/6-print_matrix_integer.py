285eb2e420165efcf7e601eed77ef57cd4e7f825
KenNyagz <kennyagz681@gmail.com>
2023-10-09 11:45:50 +0300

    An aliased Commit to save time

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,16 @@
 def element_lengths(matrix=[[]]):
     return [len(str(element)) for row in matrix for element in row]
 
+
 def alt_max(seq):
+    if not seq:
+        return
     mx = seq[0]
     for item in seq:
         if item > mx:
             mx = item
     return mx
+
 
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
