cf5165c9476c28916485c687e4b5461e11411038
KenNyagz <kennyagz681@gmail.com>
2023-10-10 20:26:42 +0300

    simp calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
-        print(f"Usage:  {sys.argv[0]} <a> <operator> <b>")
+        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
