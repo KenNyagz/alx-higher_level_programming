713e3c8ed8bf2e0bd1b3111487b04951c71a9555
KenNyagz <kennyagz681@gmail.com>
2023-10-10 15:49:13 +0300

    Simp calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,14 +5,17 @@ if __name__ == "__main__":
     import calculator_1
     if len(sys.argv) != 4:
         print("Usage:  ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit (1)
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if sys.argv[2] == "+":
-        print(calculator_1.add(int(sys.argv[1]), int(sys.argv[3])))
+        print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
     elif sys.argv[2] == "-":
-        print(calculator_1.sub(int(sys.argv[1]), int(sys.argv[3])))
+        print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
     elif sys.argv[2] == "*":
-        print(calculator_1.mul(int(sys.argv[1]), int(sys.argv[3])))
+        print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
     elif sys.argv[2] == "/":
-        print(calculator_1.div(int(sys.argv[1]), int(sys.argv[3])))
+        print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
