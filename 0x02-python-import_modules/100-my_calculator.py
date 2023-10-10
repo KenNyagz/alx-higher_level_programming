ce515ca294b22c6bf3ac48da1462b0c6e5ad4763
KenNyagz <kennyagz681@gmail.com>
2023-10-10 20:23:47 +0300

    simp calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     import sys
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
         print(f"Usage:  {sys.argv[0]} <a> <operator> <b>")
         sys.exit(1)
@@ -10,17 +10,17 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
     op = sys.argv[2]
     if sys.argv[2] == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif sys.argv[2] == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif op == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif sys.argv[2] == "/":
         if b == 0:
             print("Error: Division by zero")
             sys.exit(1)
         else:
-            print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
