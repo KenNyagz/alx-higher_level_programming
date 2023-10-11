96c88f035717b2ed2920148148151fc2bc788582
KenNyagz <kennyagz681@gmail.com>
2023-10-11 14:46:21 +0300

    Squares of matrixes

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 
 
 def square_matrix_simple(matrix=[]):
-     squarefunc = lambda x : x * x
-     newmatrix = [[squarefunc(col) for col in row] for row in matrix]
-     return newmatrix
+    squarefunc = lambda x: x * x
+    newmatrix = [[squarefunc(col) for col in row] for row in matrix]
+    return newmatrix
