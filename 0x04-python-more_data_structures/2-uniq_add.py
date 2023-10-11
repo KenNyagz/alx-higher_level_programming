a19e784956814d5af3f1c9ac876762a593f01d1b
KenNyagz <kennyagz681@gmail.com>
2023-10-11 17:00:10 +0300

    An aliased Commit to save time

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/pyton3
 
-from functools import reduce
+
 def uniq_add(my_list=[]):
-    return reduce((lambda x, y : x + y), set(element for element in my_list))
+    uniques = set(element for element in my_list)
+    return sum(uniques)
