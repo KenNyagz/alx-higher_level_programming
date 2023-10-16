f8ba119988516fa67a736a044a4ef1256f665563
KenNyagz <kennyagz681@gmail.com>
2023-10-16 03:12:42 +0300

    Calc weighted score

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,4 +2,13 @@
 
 
 def weight_average(my_list=[]):
-    
+    if not my_list:
+        return 0
+    total_score = 0
+    total_weight = 0
+
+    for score, wight in my_list:
+        total_score += score * weight
+        total_weight += weight
+
+    return total_score /  total_weight
