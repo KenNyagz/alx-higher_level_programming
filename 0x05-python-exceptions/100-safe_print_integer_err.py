8d84ecfd71ea1fc34b08a860441adc051c482a85
KenNyagz <kennyagz681@gmail.com>
2023-10-23 22:49:41 +0300

    Redirect exception to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 
+import sys
 def safe_print_integer_err(value):
     try:
         intval = int(value)
         print("{:d}".format(intval)
         return True
-    except Exception as e:
+    except ValueError as e:
         print("Exception:", e, file=sys.stderr)
         return False
