88d2b06cf287caed9dbe1e03670d0c92913485b4
KenNyagz <kennyagz681@gmail.com>
2023-10-23 17:06:08 +0300

    An aliased Commit to save time

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,11 +6,11 @@ def safe_print_list(my_list=[], x=0):
         print()
         return 0
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end=' ')
             count += 1
-    except IndexError:
-         pass
+        except IndexError:
+            pass
     print()
     return count
