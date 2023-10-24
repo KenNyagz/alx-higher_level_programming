614e3444f1994c240743efe9706328bda05b30d5
KenNyagz <kennyagz681@gmail.com>
2023-10-24 23:37:09 +0300

    An aliased Commit to save time

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,12 +3,12 @@
 
 
 class Square:
-    """ Publi instance"""
+    """Class with Public instance method"""
     
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        def area(self, size):
-            return size ** 2
+    def area(self, size):
+        return size ** 2
