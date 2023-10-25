0d02618d9c7ff4e44ce4a9c32c558404015d1746
KenNyagz <kennyagz681@gmail.com>
2023-10-26 00:44:50 +0300

    An aliased Commit to save time

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,8 +29,9 @@ class Square:
     @position.setter
     def position(self, value):
         """set position of object of class Square"""
-        if isinstance(value, tuple) and isinstance(value[0], int) and\
-           isinstance(value[1], int) and value[0] >= 0 and value[1] >= 0:
+        if isinstance(value, tuple) and isinstance(value[0], int) and \
+           len(value) == 2 and isinstance(value[1], int) and value[0] >= 0 and\
+           value[1] >= 0:
             self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
