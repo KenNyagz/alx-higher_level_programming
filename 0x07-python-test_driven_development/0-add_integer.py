ab3834b742c1ec5edabbe46c274d0c3e1be11618
KenNyagz <kennyagz681@gmail.com>
2023-10-27 23:07:16 +0300

    TDD py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,53 +1,14 @@
 #!/usr/bin/python3
+"""
+   add_integer - a function that adds two integers and returns teh result
+   if a either a or b is a float they are first converted to integers
+   If there is a none integer it raises an error(TypeError)
+"""
 
 
 def add_integer(a, b=98):
     """
-    add_integer(a, b=98):
-        if not isinstance(a, (int, float)):
-            raise TypeError("a must be an integer")
-        if not isinstance(b, (int, float)):
-            raise TypeError("b must be an integer")
-
-        a = int(a)
-        b = int(b)
-        return a + b
-
-    Adds two integers or wholesome floats
-
-    >>>add_integer(2, 3)
-    5
-
-    >>>add(2)
-    100
-
-    >>>add_integer(2, 3.0)
-    5
-
-    >>>add_integer(2, "d")
-    Traceback (most recent call last):
-      File "./0-add_integer.py", line 30, in <module>
-        add_integer(2, "b")
-      File "./0-add_integer.py", line 24, in add_integer
-        raise TypeError("b must be an integer")
-    TypeError: b must be an integer
-
-    >>>add_integer("a", 2)
-    Traceback (most recent call last):
-      File "./0-add_integer.py", line 35, in <module>
-        add_integer(2, "b")
-      File "./0-add_integer.py", line 43, in add_integer
-        raise TypeError("a must be an integer")
-    TypeError: a must be an integer
-
-    >>>add_integer("a", "b")
-    Traceback (most recent call last):
-      File "./0-add_integer.py", line 30, in <module>
-        add_integer(2, "b")
-      File "./0-add_integer.py", line 24, in add_integer
-        raise TypeError("b must be an integer")
-    TypeError: b must be an integer
-
+       Adds two integers and returns the result
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" """
+
+
+def matrix_divided(matrix, div):
+    """
+       Divides all elements of a matrix by a number
+       provided all the elements are either ints or doubles
+    """
+
+    if div is None:
+        return matrix
+
+    # check matrix arg is valid matrix
+    if not isinstance(matrix, list) or\
+       not all(isinstance(row, list) for row in matrix):
+        raise TypeError(
+           "matrix must be a matrix (list of lists) of integers/floats")
+
+    if not isinstance(div, (float, int)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    rowsize = len(matrix[0])
+    for row in matrix:
+        if len(row) != rowsize:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for column in row:
+            if not isinstance(column, (int, float)):
+                raise TypeError(
+                  "matrix must be a matrix (list of lists) of integers/floats")
+            else:
+                column = round((column / div), 2)
+                new_row.append(column)
+        new_matrix.append(new_row)
+
+    return new_matrix
