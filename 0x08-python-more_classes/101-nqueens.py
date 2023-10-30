4f7aa26fddb2ff4b9d20d352e526bb901f035c90
KenNyagz <kennyagz681@gmail.com>
2023-10-31 00:09:02 +0300

    nqueens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Solving nqueens problem chess
+   n being the number of queens as well as the 
+   no of cols and rows of chess board
+"""
+
+import sys
+def main:
+    """ nqueens problem"""
+
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    if not isinstance(sys.argv[1], int):
+        print("N must be a number")
+        syss.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
