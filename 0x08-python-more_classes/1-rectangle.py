47a92d371eb407ce79dbbf08accf784ba24b35d2
KenNyagz <kennyagz681@gmail.com>
2023-10-30 14:13:32 +0300

    An aliased Commit to save time

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self.width = value
 
     @property
     def height(self):
