cc1a25cfcbf2f4e69ce0f7ad97828515c8c42f9e
KenNyagz <kennyagz681@gmail.com>
2023-10-31 20:45:38 +0300

    An aliased Commit to save time

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -11,20 +11,6 @@ class LockedClass:
     __slots__ = ('first_name',)
 
     def __setattr__(self, name, value):
-        if hasattr(self, name):
-             super().__setattr__(name, value)
-        elif name == 'first_name':
-             super().__setattr__(name, value)
-        else:
-            raise AttributeError(f"'LockedClass' object has no attribute {name}'")
-
-    #@property
-    #def first_name(self):
-     #   return self.__first_name
-
-    #@first_name.setter
-    #def first_name(self, value):
-     #   self.__firt_name = value
-
-    #def __freeze(self):
-        #self.__locked = True'
+        if name != 'first_name':
+             raise AttributeError
+        super().__setattr__(name, value)
