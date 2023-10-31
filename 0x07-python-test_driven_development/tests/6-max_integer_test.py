f4b99083b1ac16fd3e0af4450abe9c3027c57db4
KenNyagz <kennyagz681@gmail.com>
2023-10-31 13:17:06 +0300

    What function would you use to get the type of an object?

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
+"""
+   Module to test functon that finds the max integer in a list
+"""
 
 
-
-def max_integer(list=[]):
+import uniittest
+class test_max_integer(unittest.TestCase):
     """
-
+       using unittest subclass to test 
     """
-    
+    # Normal use case
+    # Float
+    # Negative int
+    # None
+    # Non integers
+    # same values list
+    # Empty list
+    # pass list of lists(matrix)
+    self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+ 
+if __name__ == '__main__':
+    unittest.TestCase()
