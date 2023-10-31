c69e1bd70c0eb9302b27d79a6dece45590261c7a
KenNyagz <kennyagz681@gmail.com>
2023-10-31 19:03:51 +0300

    Locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-
+   A locked class that oe cannot create attributes dynamically
 """
 
 
 class LockedClass:
     """
-
+       Only possible attribute is first_name instance attribute
     """
     __locked = False
 
