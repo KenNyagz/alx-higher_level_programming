fed09878ac87233d98f98013141ed38fb5e0a416
KenNyagz <kennyagz681@gmail.com>
2023-10-31 23:57:08 +0300

    TDD

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -16,4 +16,4 @@ def max_integer(list=[]):
             result = list[i]
         i += 1
     return result
-max_integer(None)
+
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,21 +4,58 @@
 """
 
 
-import uniittest
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
 class test_max_integer(unittest.TestCase):
     """
        using unittest subclass to test 
     """
-    # Normal use case
-    # Float
-    # Negative int
-    # None
-    # Non integers
-    # same values list
-    # Empty list
-    # pass list of lists(matrix)
-    self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
- 
+    # pass list of lists(matrix)
+    def test_normaluse(self):
+       """Testing normal use case"""
+       self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_listWithFloat(self):
+        """Test case: list with float components"""
+        self.assertEqual(max_integer([1, 5.2, 3]), 5.2)
+
+    def test_negativeElems(self):
+        """Test case: List with negative numbers"""
+        self.assertEqual(max_integer([1, +5.2, -2.2, -3]), 5.2)
+
+    def test_None(self):
+        """Test case:when None is passed"""
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_NoneAsElem(self):
+        """Test case: test when None is passed a list member"""
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_nonints(self):
+        """Test case: list with non numerical elems"""
+        with self.assertRaises(TypeError):
+            max_integer([1, 2, "one"])
+
+    def test_samevals(self):
+        """Test case: list with same values"""
+        self.assertEqual(max_integer([2, 2, 2]), 2)
+
+    def test_emptylist(self):
+        """Test case: Empty list"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_noArgs(self):
+        """Test case: No argument passed"""
+        self.assertEqual(max_integer(), None)
+
+    def test_matrices(self):
+        """Test case: Lists of list(matrices) passed as args"""
+        with self.assertRaises(TypeError):
+            max_integer([1, [2, 3], 4])
+
 if __name__ == '__main__':
-    unittest.TestCase()
+    unittest.main()
