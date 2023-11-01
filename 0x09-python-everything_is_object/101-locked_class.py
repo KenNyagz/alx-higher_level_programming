c69cbe97cdfdd1e1efae63d983dd5df761381c4d
KenNyagz <kennyagz681@gmail.com>
2023-11-01 16:35:38 +0300

    More tdd

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,15 +8,15 @@ class LockedClass:
     """
        Only possible attribute is first_name instance attribute
     """
+    __slots__ = ('first_name',)
+   # def __setattr__(self, name, value):
+    #    if name != 'first_name' and not hasattr(self, name):
+       #     raise AttributeError("'LockedClass' object has no attribute '{}'".
+        #                         format(name))
+     #   super().__setattr__(name, value)
 
-    def __setattr__(self, name, value):
-        if not hasattr(self, name) and name != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute '{}'".
-                                 format(name))
-        super().__setattr__(name, value)
-
-    def __delattr__(self, name):
-        if name != "first_name":
-            raise AttributeError("'LockedClass' object has no attribute'{}'".
-                                 format(name))
-        # super().__delattr__(name)
+    # def __delattr__(self, name):
+      #  if name != "first_name":
+           # raise AttributeError("'LockedClass' object has no attribute'{}'".
+        #                         format(name))
+       # super().__delattr__(name)
