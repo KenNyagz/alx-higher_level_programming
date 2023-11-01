9aa427a6929b901319398368e464555504422ca6
KenNyagz <kennyagz681@gmail.com>
2023-11-01 17:23:07 +0300

    An aliased Commit to save time

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -10,12 +10,32 @@ def matrix_mul(m_a, m_b):
     """
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    elif not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
-
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    elif not all(isinstance(row, list) for row in m_b):
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
 
-    if 
+    if len(m_a) == 0:
+        raise ValueError("m_a can't be empty")
+    if len(m_b) == 0:
+        raise ValueError("m_b can't be empty")
+
+    for row in m_a:
+        for elem in row:
+            if not isinstance(elem, (float, int)):
+                raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        for elem in row:
+            if not isinstance(elem, (float, int)):
+                raise TypeError("m_b should contain only integers or floats")
+
+    len_row_a = len(mat_a[0])
+    for i in mat_a:
+        if len(mat_a[i]) != len_row_a:
+            raise TypeError("each row of m_a must be of the same size")
+    for j in mat_b:
+        if len(mat_b[i]) != len(mat_b[0]):
+            raise TypeError("each row of m_b must be of the same size")
