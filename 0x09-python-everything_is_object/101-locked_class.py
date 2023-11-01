79b67a1fb95c23468e4a254c6c706069d7e0d82d
KenNyagz <kennyagz681@gmail.com>
2023-11-01 16:37:14 +0300

    Locked class- Kumbe it was this simple

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,14 +9,3 @@ class LockedClass:
        Only possible attribute is first_name instance attribute
     """
     __slots__ = ('first_name',)
-   # def __setattr__(self, name, value):
-    #    if name != 'first_name' and not hasattr(self, name):
-       #     raise AttributeError("'LockedClass' object has no attribute '{}'".
-        #                         format(name))
-     #   super().__setattr__(name, value)
-
-    # def __delattr__(self, name):
-      #  if name != "first_name":
-           # raise AttributeError("'LockedClass' object has no attribute'{}'".
-        #                         format(name))
-       # super().__delattr__(name)
