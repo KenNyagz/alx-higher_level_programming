fb7f303f9d667e893f90db72fbc6d1b656037b85
KenNyagz <kennyagz681@gmail.com>
2023-11-02 13:04:40 +0300

    Square each matrix element

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,4 +2,12 @@
 
 
 def square_matrix_map(matrix=[]):
+    cp_matrix = []
+    for row in matrix:
+        cp_row = []
+        for elm in row:
+            cp_elm = elm ** 2 
+            cp_row.append(cp_elm)
+        cp_matrix.append(cp_row)
 
+    return cp_matrix
