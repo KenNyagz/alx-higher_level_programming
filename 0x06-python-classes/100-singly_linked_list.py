fcc17ab4c1896b72b83c5ceb309f575f611fc81c
KenNyagz <kennyagz681@gmail.com>
2023-11-06 00:51:50 +0300

    Singly linked list implementaion in python style OOP

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,14 +6,17 @@ class Node:
     """Singly linked list in python"""
 
     def __init__(self, data, next_node=None):
+    """Initializes a linked list node"""
         self.__data = data
         self.__nextnode = nextnode
 
     @property
     def data(self):
+    """Gets data inside node"""
         return self.__data
 
     @data.setter
+    """Sets data inside node"""
     def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
@@ -21,10 +24,12 @@ class Node:
             self.__data = value
 
     @property
+    """Retrieves pointer to next node inside a node"""
     def next_node(self):
         return self.__next_node
 
     @next_node.setter
+    """Sets pointer to next node inside the node"""
     def next_node(self, value):
          if value == None or isinstance(value, Node):
              self.__next_node = value
@@ -43,13 +48,16 @@ class SinglyLinkedList:
 
 
     def __init__(self):
+    """Initialises a singly linked list"""
         self.__head = None
 
     while True:
+    """Prints out linked list"""
         print (head.__data(), file=stdout)
         self.__head = head.__next_node
 
     def sorted_insert(self, value):
+    """Inserts node into the linked list at appropriate position"""
         while self.__data < head.__data:
             self.__head = head.__next_node
          
