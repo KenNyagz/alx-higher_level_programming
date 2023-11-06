e26967103fa0139894f321380a958495e84b9c5d
KenNyagz <kennyagz681@gmail.com>
2023-11-06 22:59:14 +0300

    Single inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+A class BaseGeometry
+That validates only integers
+Raises exception if not ints
+"""
+
+
+class BaseGeometry:
+    """
+    Class base geometry
+    Check for integers, if not raises Exception
+    """
+    def area(self):
+        """
+        raises an Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".formart(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".formart(name))
+
+
+"""
+class Rectangle that inherits from BaseGeometry   
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+
+    """
+    def __init__(self, width, height):
+        """Initializes Rectangle base on class BaseGeometry"""
+        super().integer_validator(width, height)
+        self.__width = width
+        self.__height = height
