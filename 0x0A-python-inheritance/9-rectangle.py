64bd74cff31b430b78f9c136edf2567b7008a62a
KenNyagz <kennyagz681@gmail.com>
2023-11-07 00:28:55 +0300

    Fn overwrite

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+       class Rectangle that inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """Initializes Rectangle based on class BaseGeometry"""
+        # super().integer_validator(width, height)
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ Calculates the area of a rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Defines format in which print fn displays"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def __repr__(self):
+        """Outputs human readable representation of class object"""
+        return self.__str__()
