896b4875eecd091014e20bc452e98e4da1a84141
KenNyagz <kennyagz681@gmail.com>
2023-11-07 00:30:30 +0300

    Fn overwrite

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""
+   class Rectangle that inherits from BaseGeometry
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-    class Rectangle that inherits from BaseGeometry
-"""
+
 
 
 class Rectangle(BaseGeometry):
