d3ff3e521773d4c651e47ecc6fec4866195a5e45
KenNyagz <kennyagz681@gmail.com>
2023-11-07 23:49:40 +0300

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -12,13 +12,10 @@ def class_to_json(obj):
     """
     if isinstance(obj, (list, dict, int, str, bool)):
         return obj
-    # elif isinstance(obj, list):
-      # return [class_to_json(item) for item in obj]
-    # elif isinstance(obj, dict):
-      #  return {key: class_to_json(value) for key, value in obj.items()}
     elif isinstance(obj, (tuple, set)):
         return list(obj)
     elif hasattr(obj, '__dict__'):
         return {key: class_to_json(val) for key, val in obj.__dict__.items()}
     else:
-        raise TypeError("Object of type {} is not JSON serializable".format(type(obj).__name__))
+        raise TypeError("Object of type {} is not JSON
+                        serializable".format(type(obj).__name__))
