6fd4cce5f4f20ca25bebb777fbed1d02aa347690
KenNyagz <kennyagz681@gmail.com>
2023-11-07 23:47:05 +0300

    adds all arguments to Python list, and then save them to a file

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+function that returns the dictionary description with simple data structure
+(list, dictionary, string, integer and boolean) for JSON serialization of obj
+"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    for JSON serialization of object
+    """
+    if isinstance(obj, (list, dict, int, str, bool)):
+        return obj
+    # elif isinstance(obj, list):
+      # return [class_to_json(item) for item in obj]
+    # elif isinstance(obj, dict):
+      #  return {key: class_to_json(value) for key, value in obj.items()}
+    elif isinstance(obj, (tuple, set)):
+        return list(obj)
+    elif hasattr(obj, '__dict__'):
+        return {key: class_to_json(val) for key, val in obj.__dict__.items()}
+    else:
+        raise TypeError("Object of type {} is not JSON serializable".format(type(obj).__name__))
