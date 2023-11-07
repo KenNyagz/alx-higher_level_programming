73ebbf06324297020a893f6212d690f45a740ffe
KenNyagz <kennyagz681@gmail.com>
2023-11-07 15:27:52 +0300

    reads a text file (UTF8) and prints it

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    def read_file(filename=""):
+    """
+    with open(filename, "r") as data:
+        rd_data = data.read()
+        print(rd_data)
