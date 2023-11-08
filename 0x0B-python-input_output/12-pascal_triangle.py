556e349b08027598a19eec8b040717235cc7a58e
KenNyagz <kennyagz681@gmail.com>
2023-11-08 15:24:44 +0300

    Pascal Triangle py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,12 +13,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    pasctri = [[0]*n for col in range(n)]
+    pasctri = [[1]]
 
-    for i in range(n):
-        for j in range(i+1):
-            if j == 0 or j == i:
-                pasctri[i][j] = 1
-            else:
-                pasctri[i][j] = pasctri[i-1][j-1] + pasctri[i-1][j]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(pasctri[i-1][j-1] + pasctri[i-1][j])
+        row.append(1)
+        pasctri.append(row)
     return pasctri
