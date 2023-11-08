2b0f091e6105c90cf680c15247989d421ad9e156
KenNyagz <kennyagz681@gmail.com>
2023-11-08 21:27:58 +0300

    Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,17 +12,13 @@ def append_after(filename="", search_string="", new_string=""):
     """
     tfilename = filename + ".tmp"
 
-    with open(filename, 'r+') as fle:
-        lines = fle.readlines()
-        newlines = []
+    with open(filename, 'r+', encoding='utf-8') as fle:
+        st = ""
 
-        for line in lines:
+        for line in fle:
+            st += line
             if search_string in line:
-                newlines.append(line)
-                newlines.append(new_string)
-            else:
-                newlines.append(line)
+                st += new_string
 
         fle.seek(0)
-        fle.truncate()
-        fle.writelines(newlines)
+        fle.write(st)
