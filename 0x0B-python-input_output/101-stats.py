c808069900505166f6e4ec10a2cbf3b5760ac80d
KenNyagz <kennyagz681@gmail.com>
2023-11-09 09:49:37 +0300

    Log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import sys
+import signal
+
+metrics = {
+           'totalsize': 0,
+           'status_code': {200: 0, 301: 0, 400: 0, 401: 0,
+                           403: 0, 404: 0, 405: 0, 500: 0}
+          }
+
+def print_metrics():
+    print(f"Total file size: {metrics['totalsize']}")
+    for code, count in sorted(metrics['status_code'].items()):
+        if count > 0:
+            print(f"{code}: {count}")
+
+def signal_handler(sig, frame):
+    print_metrics()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+try:
+    linecount = 0
+    for line in sys.stdin:
+        linecount += 1
+        parts = line.split()
+        filesize = int(parts[-1])
+        status_code = int(parts[-2])
+
+        metrics['totalsize'] += filesize
+        if status_code in metrics['status_code']:
+            netrics['status_code'][status_code] += 1
+
+        if linecount % 10 == 0:
+            print_metrics()
+
+except KeyboardInterrupt:
+    print_metrics()
