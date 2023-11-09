b9c415f518db301dc6092c2571e411ed403ce297
KenNyagz <kennyagz681@gmail.com>
2023-11-09 11:08:40 +0300

    Log pasrsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,46 +1,37 @@
 #!/usr/bin/python3
 """
-script that reads stdin line by line and computes metrics
-prints the metrics
+
+
 """
-
 import sys
-import signal
 
-metrics = {
-           'totalsize': 0,
-           'status_code': {200: 0, 301: 0, 400: 0, 401: 0,
-                           403: 0, 404: 0, 405: 0, 500: 0}
-          }
 
-def print_metrics():
-    """prints the stats using a specified format"""
-    print(f"Total file size: {metrics['totalsize']}")
-    for code, count in sorted(metrics['status_code'].items()):
-        if count > 0:
-            print(f"{code}: {count}")
+def printstats(stats):
+    """  """
+    totalsize = sum(stats.values())
+    print("Total file size: File size: {}".format(totalsize))
+    for statuscode in sorted(stats):
+        count = stats[statuscode]
+        print("{}: {}".format(status_code, count))
 
-def signal_handler(sig, frame):
-    """Not sure tbh"""
-    print_metrics()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-try:
+def process_input():
+    stats = {}
     linecount = 0
-    for line in sys.stdin:
-        linecount += 1
-        parts = line.split()
-        filesize = int(parts[-1])
-        status_code = int(parts[-2])
 
-        metrics['totalsize'] += filesize
-        if status_code in metrics['status_code']:
-            netrics['status_code'][status_code] += 1
+    try:
+        for line in sys.stdin:
+            linecount += 1
+            ip, _, _, _, statuscode, filesize = line.split()[0:6]
 
-        if linecount % 10 == 0:
-            print_metrics()
+            if statuscode in status:
+                stats[statuscode] += 1
+            else:
+                stats[statuscode] = 1
 
-except KeyboardInterrupt:
-    print_metrics()
+            if linecount % 10 == 0:
+                printstats(stats)
+                print()
+    except KeyboardInterrupt:
+        printstats(stats)
+
+process_input()
