3a43460f03251c1a4bdb3674275d2d49e5c272a6
KenNyagz <kennyagz681@gmail.com>
2023-11-10 21:59:07 +0300

    Inheritance

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,4 +10,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initialiser"""
         super().__init__(size, size, x, y, id)
-        
+
+    def __str__(self):
+        """Print format"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
