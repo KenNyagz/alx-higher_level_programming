f909ba14a3ea79481cfab0c4c473987e74319733
KenNyagz <kennyagz681@gmail.com>
2023-12-13 23:53:59 +0300

    number of records

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- list the number of records with the same score
 
-SELECT score, COUNT(*) AS record_count
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-ORDER BY record_count DESC;
+ORDER BY number DESC;
