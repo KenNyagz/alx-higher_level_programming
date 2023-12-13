4dab2bafe04596f2639d03741ad8b2959ce261f4
KenNyagz <kennyagz681@gmail.com>
2023-12-13 22:07:18 +0300

    second tbl

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
 -- create a second table in a db and add multiple rows
 
-CREATE IF NOT EXISTS `second_table`(`id` INT, `name` VARCHAR(256), `score` INT);
+CREATE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 VALUES(1,'JOHN', 10), (2, 'ALEX', 3), (3, 'BOB', 14), (4, 'GEORGE', 8);
