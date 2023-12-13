88aee00c7f90217d5cefbd24aaf301caf26db1a5
KenNyagz <kennyagz681@gmail.com>
2023-12-14 00:06:14 +0300

    NOT NULL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,6 @@
 -- cript that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 
-SELECT * FROM second_table
+SELECT score, name
+FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
