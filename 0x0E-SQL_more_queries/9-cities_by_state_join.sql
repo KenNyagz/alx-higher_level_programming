1559280fac158a2be1b3e6921fa6bef278a87316
KenNyagz <kennyagz681@gmail.com>
2023-12-14 19:00:15 +0300

    all cities in db

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,6 +3,6 @@
 USE hbtn_0d_usa;
 
 SELECT cities.id, cities.name AS city_name, states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = states.id
+FROM hbtn_0d_usa.cities
+JOIN htbtn_0d_usa.states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
