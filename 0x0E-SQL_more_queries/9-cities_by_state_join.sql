3cff2894bdb7fc49e680c25017c0fcead75f2d6e
KenNyagz <kennyagz681@gmail.com>
2023-12-14 19:00:46 +0300

    all cities in db

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,5 @@
 --  lists all cities contained in the database hbtn_0d_usa
 
-USE hbtn_0d_usa;
-
 SELECT cities.id, cities.name AS city_name, states.name AS state_name
 FROM hbtn_0d_usa.cities
 JOIN htbtn_0d_usa.states ON cities.state_id = states.id
