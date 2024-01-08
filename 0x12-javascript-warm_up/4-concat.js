bfa1611e111c4d060f5b151ddc88cb3866dbf099
KenNyagz <kennyagz681@gmail.com>
2024-01-08 20:50:02 +0300

    str concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,10 +2,10 @@
 
 const cmdArgs = process.argv;
 
-if (cmdArgs[2] === undefined){
-  cmdArgs[2] = "undefined";
+if (cmdArgs[2] === undefined) {
+  cmdArgs[2] = 'undefined';
 }
-if (cmdArgs[3] === undefined){
-  cmdArgs[3] = "undefined";
-} 
-console.log(cmdArgs[2] + " is " + cmdArgs[3]);
+if (cmdArgs[3] === undefined) {
+  cmdArgs[3] = 'undefined';
+}
+console.log(cmdArgs[2] + ' is ' + cmdArgs[3]);
