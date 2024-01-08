ce5426502a848841d09cd764b12e2c75640b4d81
KenNyagz <kennyagz681@gmail.com>
2024-01-08 20:49:13 +0300

    An aliased Commit to save time

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -8,4 +8,4 @@ if (cmdArgs[2] === undefined){
 if (cmdArgs[3] === undefined){
   cmdArgs[3] = "undefined";
 } 
-console.log(cmdArgs[2], ' is ', cmdArgs[3]);
+console.log(cmdArgs[2] + " is " + cmdArgs[3]);
