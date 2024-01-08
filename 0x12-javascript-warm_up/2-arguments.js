e2572e7cd78182b09993e20abd058a2f8f61f441
KenNyagz <kennyagz681@gmail.com>
2024-01-08 20:17:14 +0300

    cmdline args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,9 +5,9 @@ const args = process.argv;
 const numArgs = args.length - 2;
 
 if (numArgs === 0) {
-	console.log("No argument");
+  console.log('No argument');
 } else if (numArgs === 1) {
-	console.log("Argument found");
+  console.log('Argument found');
 } else {
-	console.log("Arguments found");
+  console.log('Arguments found');
 }
