ee6d1ccb38cde3da6d375a779938be7316d8ff6d
KenNyagz <kennyagz681@gmail.com>
2024-01-08 22:31:42 +0300

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (isNaN(num) || num < 0) {
+    return 1;
+  }
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+const args = process.argv;
+
+const num = parseInt(args[2]);
+
+if (!isNaN(num)) {
+  console.log(factorial(num));
+}
