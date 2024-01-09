d9e56ae3590c091527a6e81e04941d815330d2bd
KenNyagz <kennyagz681@gmail.com>
2024-01-09 21:14:33 +0300

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
 
-const Square = require('./5-square.js');
+const Square2 = require('./5-square.js');
 
-class Square extends Square {
-
+class Square extends Square2 {
   constructor (size) {
     super(size, size);
   }
 
-  charPrint(c) {
-  if (Object.keys(this).length === 0) {
+  charPrint (c) {
+    if (Object.keys(this).length === 0) {
       console.log('');
     } else {
-        let chr = c;
-        if (c === undefined) {
-           c = "X";
-        }
-        for (let i = 0; i < this.height; i++) {
-          console.log(chr.repeat(this.width));
-        }
+      let chr = c;
+      if (c === undefined) {
+        chr = 'X';
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(chr.repeat(this.width));
+      }
     }
   }
 }
