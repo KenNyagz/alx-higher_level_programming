939375dc9732691874974d3e2683e5b6b8dd6bfc
KenNyagz <kennyagz681@gmail.com>
2024-01-13 21:22:38 +0300

    Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" script that lists all cities from the database hbtn_0e_4_usa"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    """lists all cities from db"""
+    if (len(sys.argv) < 4):
+        sys.exit(1)
+
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 user=sys.argv[1],
+                                 passwd=sys.argv[2],
+                                 db=sys.argv[3]
+                                 )
+    cursor = connection.cursor()
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    connection.close()
