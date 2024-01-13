3fe58be901eb902bf15bf701a9f483a11234bde9
KenNyagz <kennyagz681@gmail.com>
2024-01-13 09:13:52 +0300

    An aliased Commit to save time

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,31 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
-
-import MySQLdb
+"""lists all states from database hbtn_0e_0_usa"""
 import sys
+import MySQLdb
 
-# if __name__ == "__main__":
-def main (argv):
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
 
-    # Extract cmd-line args
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+def main(argv):
+    """main function"""
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
+    if len(argv) - 1 != 3:
+        print("Must enter 3 arguments")
+        return
 
-    # Create cursor object to execute sql queries
-    cursor = db.cursor()
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Execute MySQL qurery to fetch all states and sort by states.id
-    states = cursor.fetchall()
+    for row in cur.fetchall():
+        print(row)
 
-    # Display results
-    for state in states:
-        print(state);
-
-    cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
+
     import sys
     main(sys.argv)
