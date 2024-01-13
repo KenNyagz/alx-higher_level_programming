b1e611fb20457466360ba7d2bd566457a7231fa7
KenNyagz <kennyagz681@gmail.com>
2024-01-13 17:31:03 +0300

    An aliased Commit to save time

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,5 +2,30 @@
 """script that takes in arguments and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argmnt; but safe from MySQL injections
 """
+import sys
+import MySQLdb
 
 
+if __name__ == "__main__":
+    """displays all values in the states table of hbtn_0e_0_usa"""
+    if (len(sys.argv) < 5):
+        sys.exit(1)
+
+    db = MySQLdb.connect(port=3306,
+                         host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         )
+    state_name = sys.argv[4]
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    result = cursor.fetchall()
+    for row in result:
+        if row[1] == sys.argv[4]:
+            print(row)
+
+    cursor.close()
+    db.close()
