81e80a2e31dadc810e96235c9bbdf5a313ad2144
KenNyagz <kennyagz681@gmail.com>
2024-01-14 11:18:59 +0300

    An aliased Commit to save time

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,8 +2,10 @@
 """ contains the class definition of a State and 
     an instance Base = declarative_base()
 """
-import SQLAlchemy
+from SQLAlchemy import Column, Integer, String, create_engine
 from SQLAlchemy.ext.declarative import declarative_base
+from SQLAlchemy.orm import sessionmaker
+
 
 Base = declarative_base()
 class State(Base):
@@ -12,3 +14,8 @@ class State(Base):
     
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+engine = create_engine('mysql+mysqldb://root:@Kennyaga@localhost:3306/hbtn_0e_4_usa)')
+Base.metadata.create_all(engine)
+session = sessionmaker(bind=engine)
+session = session()
