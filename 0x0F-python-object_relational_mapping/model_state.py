a7bea7b8fab974c0df311ebcca5891a8e900c87e
KenNyagz <kennyagz681@gmail.com>
2024-01-14 12:18:50 +0300

    declarative base

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-""" contains the class definition of a State and 
+""" contains the class definition of a State and
     an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 Base = declarative_base()
+
+
 class State(Base):
     """Our States ORM class"""
     __tablename__ = "states"
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-#engine = create_engine("mysql+mysqldb://root:'@Kennyaga'@localhost:3306/hbtn_0e_4_usa")
-#Base.metadata.create_all(engine)
-#session = sessionmaker(bind=engine)
-#session = session()
