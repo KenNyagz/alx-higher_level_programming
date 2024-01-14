e9ae1d3504a238ff37cbc3b2059f737b18af15ca
KenNyagz <kennyagz681@gmail.com>
2024-01-14 14:15:30 +0300

    .first method

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -3,9 +3,11 @@
 """
 import sys
 from model_state import Base, State
-
+from urllib.parse import quote_plus  # to escape special charct '@' in passwd
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    passwd = sys.argv[2]
+    passwd_escaped = quote_plus(passwd)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], passwd_escaped, sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
