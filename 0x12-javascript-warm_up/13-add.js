375b96fcff97bb01a02cbcc61e64a518be0525fe
KenNyagz <112327222+KenNyagz@users.noreply.github.com>
2024-01-25 00:31:31 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/13-add.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const add = (num1, num2) => {
-  return num1 + num2;
-};
-
-module.exports = { add };
--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const cmdArgs = process.argv;
-
-if (cmdArgs[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(cmdArgs[2]);
-}
