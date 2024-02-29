d89ed7d47de98d8efe7df4c5632de3bbc5092ad7
KenNyagz <kennyagz681@gmail.com>
2024-03-01 00:24:27 +0300

    POST req

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send POST req to url, disp response
-curl -s -X POST -d "email=test&gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
