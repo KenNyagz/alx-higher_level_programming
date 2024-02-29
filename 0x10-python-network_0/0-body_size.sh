0d2a2b4665f72f8591f0f80826c8097ed604794d
KenNyagz <kennyagz681@gmail.com>
2024-02-29 19:36:06 +0300

    send req to url

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/bash
 # sends request to a specified url and displays the size of the body
-
-# Ensure URL is provided
-if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-response=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-
-echo "$response"
+response=$(curl -s -o /dev/null -w "%{size_download}" "$1");echo $response
