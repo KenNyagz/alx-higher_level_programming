845b506775cef51f26f6476206f4a09abb27455c
KenNyagz <kennyagz681@gmail.com>
2024-02-29 23:02:41 +0300

    send GET req to url, display response body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send GET req to URL, display body of Response
-curl -s -o response.txt -w "%{http_code}" $1 | grep "^HTTP.* 200" && cat response.txt && rm response.txt
+curl -s -o response_body.txt -D headers.txt $1 && grep -q "^HTTP.* 200" headers.txt && cat response_body.txt && rm response_body.txt headers.txt
