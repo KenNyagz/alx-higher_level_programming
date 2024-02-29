7b9422b8a07ac30a3e718dc4794f63ad4980b99b
KenNyagz <kennyagz681@gmail.com>
2024-03-01 01:23:23 +0300

    hhtp POST

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # send POST req to url, display response
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+#!/bin/bash
+curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
