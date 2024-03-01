2918a61bffc29e2352ccb54dac6daa1b33ca166a
KenNyagz <kennyagz681@gmail.com>
2024-03-01 17:56:58 +0300

    An aliased Commit to save time

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 '''sends a POST request to a passed URL and email,displays response body
 '''
-import urllib.parse
-import urllib.request
+from urllib import parse, request
 import sys
 
+def send_post_request(url, email):
+    # Prepare the data to be sent in the POST request
+    data = parse.urlencode({'email': email}).encode()
+    
+    # Create a request object
+    req = request.Request(url, data=data) # this will make the method "POST"
+    
+    # Send the request and receive the response
+    with request.urlopen(req) as response:
+        # Read the response body and decode it
+        response_body = response.read().decode('utf-8')
+        print(response_body)
+
 if __name__ == "__main__":
+    # Extract the URL and email from the command line arguments
     url = sys.argv[1]
     email = sys.argv[2]
-
-    data_ = urllib.parse.urlencode({'email': email})
-    data_ = data_.encode('utf-8')
-    reqst = urllib.request.Request(url, data_)
-
-    with urllib.request.urlopen(reqst) as resp:
-        cont = resp.read().decode('utf-8')
-        print(body)
+    
+    # Call the function to send the POST request
+    send_post_request(url, email)
