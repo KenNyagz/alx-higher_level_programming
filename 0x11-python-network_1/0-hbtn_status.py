93446c0a1b472e92f4cdfa17ca145256d334f71d
KenNyagz <kennyagz681@gmail.com>
2024-03-01 13:32:55 +0300

    what's the Status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    bytes_read = response.read()
+
+print("Body response:")
+print("\t- type:", type(bytes_read))
+print("\t- content:", bytes_read)
