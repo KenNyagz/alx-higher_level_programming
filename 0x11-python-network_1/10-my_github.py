54c1018c9d274b1c24b13c4de2f7bdd0776287ae
KenNyagz <kennyagz681@gmail.com>
2024-03-02 12:01:53 +0300

    Basic Authentication http

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,4 +5,19 @@ import requests
 import sys
 
 
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passwd = sys.argv[2]
 
+    url = 'https://api.github.com/user'
+
+    auth = (username, passwd)
+    headers = {'Accept': "application/vnd.github.v3+json"}
+    
+    response = requests.post(url, auth=auth)
+
+    if response.status_code == 200:
+        user_info = response.json() # Parse the response JSON
+        print(user_info['id'])
+    else:
+        print('None')
