b3c8228a5a1e14d12b30b3e66b817fd5fd83dee7
KenNyagz <kennyagz681@gmail.com>
2024-03-02 15:14:20 +0300

    fetch github commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,18 +8,19 @@ if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
 
-    url = f"https://github.com/{owner}/{repo}/commits"
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
 
     params = {'per_page': 10}
-    response = requests.get(url, params=params)
-    print(response.json())
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+
+    response = requests.get(url, params=params, headers=headers)
 
     if response.status_code == 200:
         commits = response.json()
-        print(commits)
         for commit in commits:
-            commit_hash = commit['commit']
-            author = commit_hash['author']['name']
+            commit_data = commit['commit']
+            commit_hash =commit['sha']
+            author = commit_data['author']['name']
             print(f'{commit_hash}: {author}')
     else:
         print("error")
