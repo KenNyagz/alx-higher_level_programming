20bbd1fc2f402e3b821264f86fdee8c1bd89c2bf
KenNyagz <kennyagz681@gmail.com>
2024-03-26 18:40:13 +0300

    Write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.error('Error:', err);
+    return;
+  }
+});
