0844d9b8bf4eb2a05555047078c3f7108231a78e
KenNyagz <kennnyagz681@gmail.com>
2024-03-26 06:30:12 -0700

    Readfile

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[1];
 
+const filePath = process.argv[2];
+
+// Read the file asynchronously
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
+    console.error('Error:', err);
+    return;
   }
+  console.log('File content:', data);
 });
