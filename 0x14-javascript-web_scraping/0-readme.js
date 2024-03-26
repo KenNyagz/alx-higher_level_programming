a231780b5448b94f43b8e60bb2b02018995a5c4c
KenNyagz <kennyagz681@gmail.com>
2024-03-26 16:45:52 +0300

    The checker nowadays is not giving me peace

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,12 +7,8 @@ const filePath = process.argv[2];
 // Read the file asynchronously
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    if (err.code === 'ENOENT') {
-      console.error(`Error: File '${err.path}' not found.`);
-    } else {
-      console.error('Error:', err);
-    }
-    return;
+    console.error('Error:', err);
+  } else {
+    console.log('File content:', data);
   }
-  console.log('File content:', data);
 });
