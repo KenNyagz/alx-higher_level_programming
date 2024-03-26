31134665d052a6b22bffdb85066facb7c680789a
KenNyagz <kennyagz681@gmail.com>
2024-03-26 16:47:19 +0300

    An aliased Commit to save time

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+const fileSys = require('fs');
+const filename = process.argv[2];
 
-const filePath = process.argv[2];
-
-// Read the file asynchronously
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error:', err);
+fileSys.readFile(filename, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
   } else {
-    console.log('File content:', data);
+    console.log(content);
   }
 });
+
