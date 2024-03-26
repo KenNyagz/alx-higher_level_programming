9eb699719ce0cc914448854da9bde15b77652ede
KenNyagz <kennyagz681@gmail.com>
2024-03-26 21:18:45 +0300

    Get status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
 
 const url = process.argv[2];
 
-request.get(url, (error, response) =>  {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  console.log(response.StatusCode)
-});
+if (url) {
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+    } else {
+      console.log(`code: ${response.statusCode}`);
+    }
+  });
+} else {
+  console.error('Please provide a URL as an argument');
+}
