64a11155e660427f24a7d8d0ef27988aa80f5563
KenNyagz <kennyagz681@gmail.com>
2024-03-26 16:43:28 +0300

    An aliased Commit to save time

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,7 +7,11 @@ const filePath = process.argv[2];
 // Read the file asynchronously
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.error('Error:', err);
+    if (err.code === 'ENOENT') {
+      console.error(`Error: File '${err.path}' not found.`);
+    } else {
+      console.error('Error:', err);
+    }
     return;
   }
   console.log('File content:', data);
